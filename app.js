ce794cb30dfec68590081ffcdc09adcd1e9b98d8
VivienFreb <vivien.frebourg@outlook.com>
2019-12-04 17:15:03 +0100

    Affichage résultat lors d'un appui en fonction du bouton appuyé

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-(function() {
-    var httpRequest;
-    document.getElementById("ajaxButton").addEventListener('click', makeRequest);
-
-    function makeRequest() {
-        httpRequest = new XMLHttpRequest();
-
-        if (!httpRequest) {
-            alert('Abandon :( Impossible de créer une instance de XMLHTTP');
-            return false;
-        }
-        httpRequest.onreadystatechange = alertContents;
-        httpRequest.open('GET', 'index.html');
-        httpRequest.send();
-    }
-
-    function alertContents() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                alert(httpRequest.responseText);
-            } else {
-                alert('Il y a eu un problème avec la requête.');
+(function () {
+    const links = document.querySelectorAll('.eleve');
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            const httpRequest = new XMLHttpRequest();
+            if (!httpRequest) {
+                alert('Abandon :( Impossible de créer une instance de XMLHTTP');
+                return false;
             }
-        }
+            httpRequest.onreadystatechange = function () {
+                if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                    if (httpRequest.status === 200) {
+                        document.getElementById('resultat').innerHTML = httpRequest.responseText;
+                    } else {
+                        document.getElementById('resultat').innerHTML = "Il y a eu un problème avec la requête."
+                    }
+                }
+            };
+            httpRequest.open('GET', this.getAttribute('href'));
+            httpRequest.send();
+        })
     }
 })();
--- demo.php
+++ demo.php
@@ -1,12 +1,1 @@
-<html lang="fr">
-<head>
-    <title>Apprentissage Ajax</title>
-    <meta charset="UTF-8">
-</head>
-<body>
-<h1>Essais avec AJAX</h1>
-<button id="ajaxButton">Cliquez-moi.</button>
-
-<script src="app.js"></script>
-</body>
-</html>
+Oui : <?= $_GET['eleve'] ?>
