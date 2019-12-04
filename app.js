9145e02c7fae2f90cb21143cb1f4d60386e4e050
VivienFreb <vivien.frebourg@outlook.com>
2019-12-04 17:34:34 +0100

    Essai avec appel d'une fausse API Rest pour tester le JSON

--- app.js
+++ app.js
@@ -13,13 +13,24 @@
             httpRequest.onreadystatechange = function () {
                 if (httpRequest.readyState === XMLHttpRequest.DONE) {
                     if (httpRequest.status === 200) {
-                        result.innerHTML = httpRequest.responseText;
+                        const api_results = JSON.parse(httpRequest.responseText);
+                        result.innerHTML = ''
+                        const ul = document.createElement('ul');
+                        result.appendChild(ul);
+                        for(let i = 0; i < api_results.length; i++){
+                            var li = document.createElement('li');
+                            li.innerHTML = api_results[i].name;
+                            ul.appendChild(li);
+                        }
+
                     } else {
                         result.innerHTML = "Il y a eu un problème avec la requête."
                     }
                 }
             };
-            httpRequest.open('GET', this.getAttribute('href'));
+            // httpRequest.open('GET', this.getAttribute('href'));
+            // Il n'est pas toujours possible d'accéder à des ressources extérieures à cause de l'AJAx (cf. CORS)
+            httpRequest.open('GET', "https://jsonplaceholder.typicode.com/users"); // Faux JSON pour test, le site accepte le Cross Origin via ses Headers
             httpRequest.send();
         })
     }
