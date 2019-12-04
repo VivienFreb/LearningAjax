c1a2451595cc66e1d4f2d3da25a4958524c6a82d
VivienFreb <vivien.frebourg@outlook.com>
2019-12-04 16:12:49 +0100

    Appel simple d'une ressource lors d'un appui sur un bouton.

--- demo.php
+++ demo.php
@@ -1,1 +1,12 @@
-<?php
+<html lang="fr">
+<head>
+    <title>Apprentissage Ajax</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>Essais avec AJAX</h1>
+<button id="ajaxButton">Cliquez-moi.</button>
+
+<script src="app.js"></script>
+</body>
+</html>
